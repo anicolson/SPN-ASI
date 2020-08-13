750435b3cb746753c847efcae43f6d0a6a9b89a1
Aaron Nicolson <aaron.nicolson@griffithuni.edu.au>
2020-08-13 10:27:29 +1000

    Update

--- spn_asi/spn_batch_delete.py
+++
@@ -1,5 +0,0 @@
-## FILE:           batch.py
-## DATE:           2018
-## AUTHOR:         Aaron Nicolson
-## AFFILIATION:    Signal Processing Laboratory, Griffith University.
-## BRIEF:          Generates mini-batches. Creates training, and test lists.
